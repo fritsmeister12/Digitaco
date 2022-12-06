a3b8d23f5a79875a93028e14f4237c58033b3a48
fritsmeister12 <mattiekill010@gmail.com>
2022-12-06 23:08:06 +0100

    Contact changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,14 +5,14 @@ const Contact = () => {
     <section class="text-gray-600 body-font relative">
         <div class="container px-5 py-12 mx-auto">
             <div class="flex flex-col text-center w-full mb-12">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Ons</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Bent u geïnteresseerd geraakt? Stuur ons dan een bericht met uw wensen en dan gaan we uw wensen verwezenlijken. Wilt u een vraag stellen? Dan kan dat natuurlijk ook door het formulier in te vullen.</p>
             </div>
             <div class="lg:w-1/2 md:w-2/3 mx-auto">
             <div class="flex flex-wrap -m-2">
                 <div class="p-2 w-1/2">
                 <div class="relative">
-                    <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                    <label for="name" class="leading-7 text-sm text-gray-600">Naam</label>
                     <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
                 </div>
@@ -24,12 +24,12 @@ const Contact = () => {
                 </div>
                 <div class="p-2 w-full">
                 <div class="relative">
-                    <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                    <label for="message" class="leading-7 text-sm text-gray-600">Bericht</label>
                     <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                 </div>
                 </div>
                 <div class="p-2 w-full">
-                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                <button class="flex mx-auto text-white bg-orange-500 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg">Verstuur</button>
                 </div>
             </div>
             </div>
